def5adab2293fbc58fde652c2bebba9cd171f316
Brendah Edward <edwardbrendah28@gmail.com>
2025-04-21 05:50:29 +0300

    Adds BotCard

---
+++ bot-battlr/src/components/BotCard.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+function BotCard({ bot, handleClick, inArmy, dischargeBot }) {
+    const botClassColor = (botClass) => {
+        switch (botClass) {
+            case 'Medic':
+                return 'bg-success';
+            case 'Assault':
+                return 'bg-danger';
+            case 'Support':
+                return 'bg-info';
+            case 'Defender':
+                return 'bg-warning';
+            case 'Captain':
+                return 'bg-primary';
+            case 'Witch':
+                return 'bg-dark';
+            default:
+                return 'bg-secondary';
+        }
+    }
+    return (
+        <div className="card h-100">
+            <div classname="position-relative">
+                <img 
+                src={bot.avatar_url} 
+                className="card-img-top" 
+                alt={bot.name}
+                onclick={handleClick} 
+                style= {{cursor: 'pointer'}}
+                />
+
+                <button className= "btn btn-danger btn-sm position-absolute top-0 end-0 m-2"
+                onClick={(event) => {
+                    event.stopPropagation()
+                    dischargeBot(bot)}}
+                >
+                        X
+                </button>
+
+            </div>
+
+            <div className="d-flex justify-content-between align-items-center">
+                <h5 className="mb-0">{bot.name}</h5>
+                <span className={`badge ${botClassColor(bot.bot_class)}`}>
+                    {bot.bot_class}
+                </span>
+            </div>
+
+            <div className="card-body" style={{ cursor: "pointer" }} onClick={handleClick}>
+                <p className="card-text">
+                    <small>{bot.catchphrase}</small>
+                </p>
+                
+                <div className="row text-center">
+                    <div className="col-4">
+                        <div className="fw-bold">Health</div>
+                        <div>{bot.health}</div>
+                    
+                        <div className="fw-bold">Damage</div>
+                        <div>{bot.damage}</div>
+                        
+                        <div className="fw-bold">Armor</div>
+                        <div>{bot.armor}</div>
+                    </div>
+                </div>
+                
+                <div className="card text-center">{inArmy ? "Click to release" : "Click to enlist"}</div>
+            </div>
+    </div>
+  )
+
+}
+export default BotCard;
+
+
